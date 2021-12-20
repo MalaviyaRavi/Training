e13f5313a89c45c03e1f59c3b7d7674f3930c8a0
MalaviyaRavi <ravi.m@webcodegenie.com>
2021-12-20 18:46:28 +0530

    render file list with upload by

--- December/api-task/controllers/users.js
+++ December/api-task/controllers/users.js
@@ -1,3 +1,5 @@
+const CsvMetaData = require("../models/csvMetaData")
+
 exports.getLogin = function (req, res, next) {
   if (req.cookies.authToken) {
     return res.redirect("/users");
@@ -5,6 +7,48 @@ exports.getLogin = function (req, res, next) {
   res.render("pages/login");
 };
 
-exports.getUsers = function (req, res, next) {
-  res.render("pages/users");
+exports.getUsers = async function (req, res, next) {
+  let files = await CsvMetaData.aggregate([{
+      $lookup: {
+        from: "users",
+        let: {
+          userId: "$uploadBy",
+
+        },
+        pipeline: [{
+            $match: {
+              $expr: {
+                $and: [{
+                  $eq: ["$_id", "$$userId"]
+                }, ]
+              }
+            }
+          },
+          {
+            $project: {
+              email: 1,
+              _id: 0
+            }
+          }
+        ],
+        as: "uploadBy"
+      }
+    },
+    {
+      $project: {
+        Name: 1,
+        status: 1,
+        uploadBy: {
+          $arrayElemAt: ["$uploadBy", 0]
+        },
+      }
+    }
+
+
+  ])
+
+  console.log(files);
+  res.render("pages/users", {
+    files
+  });
 };
